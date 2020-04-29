b13d9be2f20797eca43aa3f76984b81e7f88368c
std-freejia <std.freejia@gmail.com>
2020-04-29 16:28:20 +0900

    initial commit april 29th

---
+++ CPP/multiple_sum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+// 1부터 N까지의 M의 배수합  
+int main(int argc, char** argv) {
+	
+	int n, m, i, sum = 0;
+	// n과 m을 입력받는다.  
+	cin >> n >> m;
+	for (i=1; i<=n; i++){
+		if(i%m == 0){
+			sum+=i;
+		} 
+	}
+	
+	cout << sum;
+	// 컴파일 f9 
+	// 실행 f10
+	return 0;
+}
