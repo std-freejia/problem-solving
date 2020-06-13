f7671ee4f7a0011039453ec791fae628aa627a9b
std-freejia <std.freejia@gmail.com>
2020-06-13 20:16:57 +0900

    43. 코드 수정.

--- CPP/43_music_video.cpp
+++ CPP/43_music_video.cpp
@@ -1,10 +1,6 @@
 #include <stdio.h>
-#include <string.h>
-#include <algorithm>
-#include <vector>
-using namespace std;
 
-// 43. 뮤직비디오   
+// 43. 뮤직비디오 
 /*
 DVD에는 총 N개의 곡이 들어가는데, DVD에 녹화할 때에는 라이브에서의 순서가 그대로 유지 되어야 한다. 
 즉, 1번 노래와 5번 노래를 같은 DVD에 녹화하기 위해서는 1번과 5번 사이의 모든 노래도 같은 DVD에 녹화해야한다.
@@ -23,61 +19,70 @@ DVD에는 총 N개의 곡이 들어가는데, DVD에 녹화할 때에는 라이브에서의 순서가 그대로
 ▣ 출력설명
 첫 번째 줄부터 DVD의 최소 용량 크기를 출력하세요.
 
-
 결정 알고리즘 
-답을 미리 정해놓고 맞는지를 확인하는 루틴.  
-맞는지 찾아가는 과정을 이분탐색으로 구현한다. 어떤 범위에 속하는지 확인하면서 한다.  
-
+답을 미리 정해놓고 맞는지를 확인하는 방식. 
+해당 범위에서 특정 답(임의로 중간값)이 답으로 타당한지 찾아가는 과정을 이분탐색으로 구현한다. 
 */
 
-// 곡  
-int a[1001],  n;
 
-int Count(int size){ // n
-	int i, cnt=1, sum = 0; // 반복문 변수, dvd 개수(최소 1개니까), 분의 합 
+int song[1001], cnt_song;
 	
-	for(i=1; i<=n; i++){ // 모든 곡 순회  
-		if(sum + a[i] > size){
-			cnt++;
-			sum = a[i]; // 새 dvd 에 들어감.  dvd의 용량보다는 노래가 작아야함. 
+int Count(int size){
+	
+	int i = 0, cnt_dvd = 1, sum = 0;
+	
+	for(i=1; i<=cnt_song; i++){ // 모든 곡 순회  
+		
+		if(sum + song[i] > size){
+			cnt_dvd++;
+			sum = song[i];
 		}else{
-			sum += a[i];
+			sum += song[i];
 		}
-	} 
-	
-	return cnt;
-}
- 
-int main(int argc, char** argv){
-	   
-	//freopen("input.txt", "rt", stdin); 
-	// max_value : 곡들 중에 최대 분.  
-	int m, i, lt=1, rt=0, mid, res, max_value = -2147000000;
-	
-	scanf("%d %d", &n, &m);
-	
-	for(i=1; i<=n; i++){ // 인덱스 1 부터 이용  
-		scanf("%d", &a[i]);
-		rt = rt + a[i]; // 곡들이 전체 몇 분인지 저장  
-		if(a[i]> max_value) max_value = a[i]; 
 	}
 	
-	// 이분 검색
-	while(lt <= rt){
-		mid = (lt + rt)/2;  // dvd 1개의 최소 용량 
-		
-		//mid 값이 가능한 용량인지 확인 
-		// mid 값은 모든 노래의 각 용량보다는 커야 한다.  
-		if(mid >= max_value && Count(mid) <= m){ // 답이 된다  
-			res = mid;
-			rt = mid-1;
-		}else{
-			lt = mid+1;
-		}
-	} 
+	return cnt_dvd;
 	
-	printf("%d", res);
-	
-	return 0;
 }
 
+
+int main(int argc, char** argv){
+	
+	int i = 0, cnt_limit_dvd = 0;
+	int lp = 1, rp = 0, mid = 0, res = 0;
+	int max_value = -2147000000;
+		
+	//freopen("input.txt", "rt", stdin);
+
+	scanf("%d %d", &cnt_song, &cnt_limit_dvd);
+	
+	// 곡들이 총 몇분인지 저장  
+	for(i=1; i<= cnt_song; i++){ // 인덱스 1 부터 사용
+		scanf("%d", &song[i]);
+		rp = rp + song[i];
+		
+		if(song[i] > max_value) max_value = song[i]; // 제일 긴 곡 저장  
+	}
+	
+	// dvd 한 개의 용량이 '제일 긴 곡'의 용량보다는 같거나 커야 한다.  
+	
+	// 가능한 dvd 의 시간 범위 : 1분 부터 rt에 저장된 분  
+	
+	//이분검색  
+	while(lp <= rp){
+		
+		mid = (lp + rp) / 2; // dvd 1개의 최소 용량 
+		
+		if(mid >= max_value && Count(mid) <= cnt_limit_dvd){ // mid 가 답이 된다.  
+			res = mid;
+			rp = mid - 1;  
+		} else {
+			lp = mid + 1; 
+		}
+	}
+	
+	printf("%d", res);
+
+	return 0;
+
+}
