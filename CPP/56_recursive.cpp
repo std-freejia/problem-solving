41901d63a16140865b538dae5175fe81a5d5f9bb
std-freejia <std.freejia@gmail.com>
2020-06-21 18:07:20 +0900

    로봇청소기 (중복 제거)

---
+++ CPP/56_recursive.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// 56. 재귀함수 분석  
+
+void Print(int num){
+	
+	if(num - 1 == 0){
+		printf("%d ", num);
+		return;
+	}else{
+		Print(num-1);
+		printf("%d ", num);
+	}
+}
+ 
+int main(int argc, char** argv){
+
+	int num;
+	
+	freopen("input.txt", "rt", stdin);
+	
+	// 입력 받기 시작
+	scanf("%d", &num);   
+	
+	Print(num);
+
+	return 0;
+}
+
---
+++ CPP/57_recursive_binary_print.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// 57. 재귀함수 이진수 출력   
+
+void Binary(int num){
+	
+	if(num/2 == 1){
+		printf("%d", num/2);
+		printf("%d", num%2);
+		return;
+	}else{
+		Binary(num/2);
+		printf("%d", num%2);
+	}
+}
+ 
+int main(int argc, char** argv){
+
+	int num;
+	
+	//freopen("input.txt", "rt", stdin);
+	
+	// 입력 받기 시작
+	scanf("%d", &num);   
+	
+	Binary(num);
+
+	return 0;
+}
+
+
