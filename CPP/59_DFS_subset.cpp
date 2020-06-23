152be97cdccccff0433cd74f59b8ed805ecaa647
std-freejia <std.freejia@gmail.com>
2020-06-23 23:31:30 +0900

    59. 부분집합

---
+++ CPP/59_DFS_subset.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <algorithm>
+#include <stack>
+#include <vector> 
+using namespace std;
+
+// 59. 부분집합  (DFS 완전탐색)
+
+
+
+int arr[11], num;
+
+void Print(){
+	
+	int i = 0;
+	
+	for(i=1; i<=num; i++){
+		if(arr[i] == 1) printf("%d", i);
+	}
+	puts("");
+	
+	return;
+}
+
+void DFS(int level){
+	
+	if(level > num ){
+		
+		// 합 체크 
+		Print(); 
+		return; 
+	}else{
+		arr[level] = 1; // 선택함  
+		DFS(level+1);
+		arr[level] = 0; // 선택안함  
+		DFS(level+1);
+	}
+
+
+	return; 
+} 
+
+int main(int argc, char** argv){
+	
+	freopen("input.txt", "rt", stdin);
+	
+	scanf("%d", &num);
+	
+	DFS(1);
+	
+	return 0;
+} 
