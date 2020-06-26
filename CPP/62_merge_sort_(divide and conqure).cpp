8f67ae4ece9cc35193ef5802911d2051757d6435
std-freejia <std.freejia@gmail.com>
2020-06-26 13:00:20 +0900

    [중요] 62. 병합정렬 ( 분할정복 )

---
+++ CPP/62_merge_sort_(divide and conqure).cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+using namespace std;
+
+// 62. 병합정렬 (분할정복) 
+ 
+ 
+int data[101], temp[101];
+ 
+void divide(int left, int right){
+	
+	int mid = 0;
+	int p1, p2, p3 = 0;  // 인덱스를 가리키는 포인트 변수. (위치변수) 
+	int i = 0;
+	
+	if(left < right){
+		
+		mid = (left + right) / 2 ;
+		
+		divide(left, mid);
+		divide(mid+1, right);
+		
+		// left~right 범위 내에서 정렬.
+		p1 = left;
+		p2 = mid+1;
+		p3 = left; // 값을 넣을 포인트  
+	
+		while(p1 <= mid && p2 <= right){ // 병합  
+			
+			if(data[p1] < data[p2]) temp[p3++] = data[p1++];
+			else temp[p3++] = data[p2++];
+			
+		}
+		
+		// 남은 것 처리 
+		while(p1 <= mid) temp[p3++] = data[p1++]; 
+		while(p2 <= right) temp[p3++] = data[p2++];
+		
+		// temp -> data 에 복사 
+		for(i = left; i<=right; i++){
+			data[i] = temp[i];
+		} 
+		
+	}else{
+		
+		return;
+	}
+	
+} 
+ 
+int main(void){
+
+	//freopen("input.txt", "rt", stdin);
+	
+	int n, i = 0;
+	
+	scanf("%d", &n);
+	
+	for(i=1; i<=n; i++){ // 인덱스는 1부터 사용.  
+		scanf("%d", &data[i]);
+	} // 입력받기 끝. 
+	
+	divide(1, n); // 정렬 재귀 함수  
+	
+	for(i=1; i<=n; i++){ // 정렬 결과 출력  
+		printf("%d ", data[i]);
+	}
+	
+	return 0;
+} 
+
+
